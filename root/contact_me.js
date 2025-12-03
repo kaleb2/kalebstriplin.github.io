dffd2de16cf6d5aa943b5309b4a31670b715a7e4
Kaleb Striplin <kaleb.striplin@thermofisher.com>
2025-12-03 14:30:38 -0800

    Adds a form to the contact seciton. Disabled email me for now.

--- root/contact_me.js
+++ root/contact_me.js
@@ -6,6 +6,13 @@ email.addEventListener("click", handleClick);
 // callback function to handle event
 function handleClick(event) {
     console.log(event);
+
+    let subject = document.querySelector("#subject");
+    console.log(subject.value);
+
+    let message = document.querySelector("#message");
+    console.log(message.value);
+
     // https://stackoverflow.com/questions/7381150/how-to-send-an-email-from-javascript
-    window.open('mailto:kaleb2@pdx.edu?subject=Hello!&body=Sup Kaleb,');
+    // window.open('mailto:kaleb2@pdx.edu?subject=Hello!&body=Sup Kaleb,');
 }
